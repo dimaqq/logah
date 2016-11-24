40035be976f3c9256bb98950772b6ef7c8b9bc0f
Dima Tisnek <dimaqq@gmail.com>
2016-11-24 22:13:11 +0100

    It works

--- naive.py
+++ naive.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import requests_unixsocket
 base_url = "http+unix://%2Fvar%2Frun%2Fdocker.sock"
 
@@ -8,15 +9,18 @@ r.raise_for_status()
 
 for line in r.iter_lines():
     doc = json.loads(line)
-    if doc["Action"] == "attach":
+    logging.debug("got event %s", doc)
+
+    if doc["Action"] == "start":
         cont = doc["id"]
         print(f"detected container {cont}")
 
         rr = s.get(f"{base_url}/containers/{cont}/logs?follow=1&stdout=1", stream=True)
         rr.raise_for_status()
         for line in rr.iter_lines():
+            logging.warn("log line %r", line)
             # doc = json.loads(line)
             # print(f"log entry {doc}")
-            print(f"got line {line!r}")
+            pass
 
         print(f"finished with {cont}")
