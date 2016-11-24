1cf6909c290cce927fb1892db8968cc8fbe9e837
Dima Tisnek <dimaqq@gmail.com>
2016-11-24 21:57:25 +0100

    Die on error

--- naive.py
+++ naive.py
@@ -4,6 +4,7 @@ base_url = "http+unix://%2Fvar%2Frun%2Fdocker.sock"
 
 s = requests_unixsocket.Session()
 r = s.get(f"{base_url}/events", stream=True)
+r.raise_for_status()
 
 for line in r.iter_lines():
     doc = json.loads(line)
@@ -12,6 +13,7 @@ for line in r.iter_lines():
         print(f"detected container {cont}")
 
         rr = s.get(f"{base_url}/containers/{cont}/logs?follow=1&stdout=1", stream=True)
+        rr.raise_for_status()
         for line in rr.iter_lines():
             # doc = json.loads(line)
             # print(f"log entry {doc}")
