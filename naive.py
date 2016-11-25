24468b926bad72c618aa0ddc2f7b9da55b5c4d68
Dima Tisnek <dimaqq@gmail.com>
2016-11-25 09:29:29 +0100

    Ignore self

--- naive.py
+++ naive.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python
 import json
 import logging
+import platform
 import requests_unixsocket
 base_url = "http+unix://%2Fvar%2Frun%2Fdocker.sock"
+
+own_short_id = platform.node()
+own_id = next((line.strip().split("/")[-1] for line in open("/proc/self/cgroup") if line.startswith("1:name=")), None)
+logging.warn("%s", [own_short_id, own_id])
 
 s = requests_unixsocket.Session()
 r = s.get(f"{base_url}/events", stream=True)
@@ -13,6 +19,10 @@ for line in r.iter_lines():
 
     if doc["Action"] == "start":
         cont = doc["id"]
+        if cont == own_id or cont.startswith(own_short_id):
+            logging.debug("Skipping self %r", cont)
+            continue
+
         print(f"detected container {cont}")
 
         rr = s.get(f"{base_url}/containers/{cont}/logs?follow=1&stdout=1", stream=True)
