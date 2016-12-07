23e191374060b50586fa72178878a3fc4a5db368
Dima Tisnek <dimaqq@gmail.com>
2016-12-07 21:35:32 +0100

    Simple synchronous aggregator

--- naive.py
+++ naive.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 import os
 import json
-import logging
 import platform
 import requests_unixsocket
 base_url = "http+unix://%2Fvar%2Frun%2Fdocker.sock"
 
-logging.basicConfig(level=logging.DEBUG if os.environ.get("DEBUG") else logging.INFO)
-
 own_short_id = platform.node()
-own_id = next((line.strip().split("/")[-1] for line in open("/proc/self/cgroup") if line.startswith("1:name=")), None)
-logging.debug("own container id %s short id %s", own_id, own_short_id)
 
 s = requests_unixsocket.Session()
 r = s.get(f"{base_url}/events", stream=True)
@@ -18,7 +13,6 @@ r.raise_for_status()
 
 for line in r.iter_lines():
     doc = json.loads(line)
-    logging.debug("got event %s", doc)
 
     if doc["Action"] == "start":
         cont = doc["id"]
@@ -27,8 +21,7 @@ for line in r.iter_lines():
         except ValueError:
             name = cont[:12]
 
-        if cont == own_id or cont.startswith(own_short_id):
-            logging.debug("Skipping self %r", cont)
+        if cont.startswith(own_short_id):
             continue
 
         print(f"detected container {cont}")
@@ -36,19 +29,6 @@ for line in r.iter_lines():
         rr = s.get(f"{base_url}/containers/{cont}/logs?follow=1&stdout=1", stream=True)
         rr.raise_for_status()
         for line in rr.iter_lines():
-            logging.debug("log line %r", line)
-            try:
-                # Good apps/containers should log in JSON
-                doc = json.loads(line)
-            except ValueError:
-                # Fall-back for old-school containers
-                try:
-                    line = line.decode("utf-8")
-                except UnicodeDecodeError:
-                    line = repr(line)
-                doc = dict(message=line)
-            
-            doc["container"] = name
-            print(json.dumps(doc))
+            print(name, line)
 
         print(f"finished with {cont}")
